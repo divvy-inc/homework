219c4806dfd319803c7de9e93c2cfa4fff5fcb8d
David Sarkisian <sarkisianvd@Davids-MacBook-Pro.local>
2021-02-12 18:11:33 -0700

    Initial commit: Setup up and test for Add/Remove Element feature

--- test/homework_test.exs
+++ test/homework_test.exs
@@ -6,7 +6,26 @@ defmodule HomeworkTest do
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "goes to google" do
-    navigate_to "http://google.com"
+  # Define setup to run before each test
+  setup do
+    # Navigate to the main page
+    navigate_to "https://the-internet.herokuapp.com/"
+    :ok
   end
+
+  test "add_remove_element" do
+    # Navigate to Add/Remove Elements page
+    add_remove = find_element(:xpath, ~s|//a[text()='Add/Remove Elements']|)
+    add_remove |> click
+
+    # Click Add Element button
+    add_element_button = find_element(:xpath, ~s|//div[@id='content']//button|)
+    add_element_button |> click()
+
+    # Verify delete button is now displayed
+    delete_button = find_element(:xpath, ~s|//div[@id='elements']//button|)
+    assert(element_displayed?(delete_button), "Delete button was not displayed")
+  end
+
+
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,4 @@
 Application.ensure_all_started(:hound)
 ExUnit.start()
+
+

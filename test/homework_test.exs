f028e71400a0fedb490664bb3a37a5be27bd8834
dollarbiehl <dollarbiehl@gmail.com>
2022-06-17 16:54:57 -0600

    added test for JS alert

--- test/homework_test.exs
+++ test/homework_test.exs
@@ -6,7 +6,17 @@ defmodule HomeworkTest do
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "goes to google" do
-    navigate_to "http://google.com"
+  test "javascript alerts" do
+    navigate_to "https://the-internet.herokuapp.com/javascript_alerts"
+
+    element = find_element(:xpath, "//*[@id='content']/div/ul/li[1]/button")
+    click(element)
+
+    assert dialog_text() == "I am a JS Alert"
+    dismiss_dialog()
+    
+    result = find_element(:id, "result")
+    assert inner_text(result) == "You successfully clicked an alert"
+    assert attribute_value(result, "style") == "color: green;"
   end
 end

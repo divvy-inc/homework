4b5b8705fe707b8312d8490846e00fe97dc843e5
dollarbiehl <dollarbiehl@gmail.com>
2022-06-17 17:09:44 -0600

    added JS confirm test for both OK and cancel

--- test/homework_test.exs
+++ test/homework_test.exs
@@ -5,18 +5,35 @@ defmodule HomeworkTest do
 
   # Start hound session and destroy when tests are run
   hound_session()
-
-  test "javascript alerts" do
+  
+  test "JS alert" do
     navigate_to "https://the-internet.herokuapp.com/javascript_alerts"
-
     element = find_element(:xpath, "//*[@id='content']/div/ul/li[1]/button")
     click(element)
 
     assert dialog_text() == "I am a JS Alert"
-    dismiss_dialog()
+    accept_dialog()
     
     result = find_element(:id, "result")
     assert inner_text(result) == "You successfully clicked an alert"
     assert attribute_value(result, "style") == "color: green;"
+  end
+  test "JS confirm" do
+    navigate_to "https://the-internet.herokuapp.com/javascript_alerts"
+    element = find_element(:xpath, "//*[@id='content']/div/ul/li[2]/button")
+    click(element)
+
+    assert dialog_text() == "I am a JS Confirm"
+
+    # click 'OK' on the dialog box
+    accept_dialog()
+    result = find_element(:id, "result")
+    assert inner_text(result) == "You clicked: Ok"
+    assert attribute_value(result, "style") == "color: green;"
+
+    # click 'Cancel' on the dialog box
+    click(element)
+    dismiss_dialog()
+    assert inner_text(result) == "You clicked: Cancel"
   end
 end

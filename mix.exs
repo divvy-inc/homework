e285a340c7ea11ca722ab6344b3e22664e236f78
dollarbiehl <dollarbiehl@gmail.com>
2022-06-18 17:27:09 -0600

    add retry package

--- mix.exs
+++ mix.exs
@@ -14,14 +14,16 @@ defmodule Homework.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      applications: [:retry]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:hound, "~> 1.0"}
+      {:hound, "~> 1.0"},
+      {:retry, "~> 0.15"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]

2d79da2efb4ee367df63a8481a48dac955aae349
dollarbiehl <dollarbiehl@gmail.com>
2022-06-20 10:55:29 -0600

    update remove/add checkbox tests

--- test/dynamic_controls_test.exs
+++ test/dynamic_controls_test.exs
@@ -19,17 +19,27 @@ defmodule DynamicControls do
   # Start hound session and destroy when tests are run
   describe "Dynamic Controls" do
     hound_session()
-    test "Remove checkbox" do
+    test "Remove/Add checkbox" do
         go_to_dynamic_controls()
         
-        checkbox = find_element(:id, "checkbox")
         remove_button = find_element(:xpath, "//*[@id='checkbox-example']/button")
+        # click the remove button
         click(remove_button)
+        # check for loading bar to display
         assert find_element(:id, "loading")
-        
+        # wait for the it's gone message to be displayed
         assert wait_for(fn -> element?(:id, "message") end)
         assert inner_text(find_element(:id, "message")) == "It's gone!"
         assert inner_text(remove_button) == "Add"
+        assert catch_error(find_element(:id, "checkbox"))
+
+        # click add button
+        click(remove_button)
+        assert find_element(:id, "loading")
+        assert wait_for(fn -> element?(:id, "message") end)
+        assert inner_text(find_element(:id, "message")) == "It's back!"
+        assert inner_text(remove_button) == "Remove"
+        assert find_element(:id, "checkbox")
     end
     
     test "enable/disable input field" do
